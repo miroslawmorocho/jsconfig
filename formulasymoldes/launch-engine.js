d4fffac944b4368e8795b960553d1f999e8a566b
miroslawmorocho <74107079+miroslawmorocho@users.noreply.github.com>
2026-03-09 17:23:21 -0500

    update launch config

--- formulasymoldes/launch-engine.js
+++ formulasymoldes/launch-engine.js
@@ -1,4 +1,77 @@
-const CONFIG = {
-  mensaje: "Hola mundo desde GitHub",
-  promo: true
-};
+const LAUNCH_ENGINE={
+
+clases:[
+
+{
+titulo:"? Resina Clase 1 ? Crea tus primeras piezas de resina desde casa",
+fecha:new Date("2026-03-08T19:30:00-05:00"),
+link:"https://www.youtube.com/live/EBr1Omb4MN4"
+},
+
+{
+titulo:"? Resina Clase 2 ? Desmoldado, decoraci?n y c?mo vender tus piezas",
+fecha:new Date("2026-03-09T19:30:00-05:00"),
+link:"https://www.youtube.com/watch?v=C7CEtHEAB7Q"
+}
+
+],
+
+cierreEvento:new Date("2026-04-03T23:59:59-05:00"),
+
+tiempos:{
+
+duracionClase:90,
+duracionCalendario:15,
+aperturaSala:5.5
+
+},
+
+oferta:{
+
+claseOferta:2,
+delayMinutos:5,
+hotlink:"https://www.formulasymoldes.com/programa-resina#comprar",
+texto:"? OFERTA ESPECIAL para {pais} {bandera}<br>37 US$ Haz Clic Aqu?"
+
+},
+
+bridge:{
+
+descripcion:"Entra a la clase haciendo clic aqu?:",
+link:"https://www.formulasymoldes.com/clases-resina"
+
+},
+
+textos:{
+
+headerAntes:"La Clase #{n} de Resina comienza en...",
+headerPronto:"La Clase #{n} de Resina comienza pronto... Ingresa a sala ?",
+headerLive:"Ya empez? la Clase #{n} de Resina... Ingresa AHORA ?",
+headerReplays:"Ya puedes ver las repeticiones de todas las clases ?",
+infoPagina:"<b>? Guarda esta p?gina.</b> Aqu? se actualiza <u>autom?ticamente</u> toda la informaci?n que necesitas saber.",
+calendarioTitulo:"A?ade {clases} a tu calendario de Google para no olvidarlo",
+calendarioNota:"<b>*</b> El aviso sonar? <b><u>30 minutos antes</u></b> de empezar",
+eventoCerrado:"Las repeticiones y la oferta ya no est?n disponibles.",
+ofertaExpira:"La OFERTA ESPECIAL del programa de Resina con Yuly desaparece el ",
+
+mensajesClase:{
+
+antes:"{fecha} ? {hora} <br> Hora de {ciudad}, {pais} {bandera}",
+pronto:"? La clase comienza pronto ? entra a sala ahora",
+live:"Ya estamos en vivo ? entra AHORA",
+replay:"? Ver repetici?n disponible"
+
+},
+
+botonesClase:{
+
+antes:"Agregar al calendario",
+pronto:"Entrar a la clase",
+live:"Ver transmisi?n",
+replay:"Ver repetici?n"
+
+}
+
+}
+
+}
