242623e494f9ffc3ea12378eafb2a81b7e168ed2
miroslawmorocho <74107079+miroslawmorocho@users.noreply.github.com>
2026-03-10 07:31:37 -0500

    update launch config

--- formulasymoldes/launch-engine.js
+++ formulasymoldes/launch-engine.js
@@ -4,19 +4,19 @@ clases:[
 
 {
 titulo:"📅 Resina Clase 1 – Crea tus primeras piezas de resina desde casa",
-fecha:new Date("2026-03-10T14:40:00-05:00"),
+fecha:new Date("2026-03-14T14:40:00-05:00"),
 link:"https://www.youtube.com/live/EBr1Omb4MN4"
 },
 
 {
 titulo:"📅 Resina Clase 2 – Desmoldado, decoración y cómo vender tus piezas",
-fecha:new Date("2026-03-10T15:36:00-05:00"),
+fecha:new Date("2026-03-14T15:36:00-05:00"),
 link:"https://www.youtube.com/watch?v=C7CEtHEAB7Q"
 }
 
 ],
 
-cierreEvento:new Date("2026-03-10T18:44:00-05:00"),
+cierreEvento:new Date("2026-03-20T18:44:00-05:00"),
 
 tiempos:{
 
