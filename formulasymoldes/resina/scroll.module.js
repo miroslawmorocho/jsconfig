93cd85c31401711f32ddef1bc7087527142a6aaa
Imperio Decorativo <miroslaw_mm@hotmail.com>
2026-03-18 02:45:34 -0500

    limpieza

--- formulasymoldes/resina/scroll.module.js
+++
@@ -1,7 +0,0 @@
-LaunchCore.globals.scroll = async function(){
-
-  const BASE = "https://miroslawmorocho.github.io/jsconfig/";
-
-  await LaunchCore.loadScript(BASE+"modules/scroll/scroll.js");
-
-};
