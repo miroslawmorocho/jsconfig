3f1deb515551c19f46f773d074c06c7efc2a425e
Imperio Decorativo <miroslaw_mm@hotmail.com>
2026-03-18 18:35:32 -0500

    cambios

--- projects/formulasymoldes/resina/bridge-module.js
+++ projects/formulasymoldes/resina/bridge-module.js
@@ -2,13 +2,19 @@ LaunchCore.register("bridge", async function(){
 
   const root = document.getElementById("launch-engine-root");
 
-  const base = "https://miroslawmorocho.github.io/jsconfig/formulasymoldes/resina/";
-
-  const html = await fetch(base + "bridge.html").then(r => r.text());
+  const base = LaunchCore.paths.projects;
+  const route = "formulasymoldes/resina/";
+  const page = root.dataset.page
+  const url = base + route + page;
+  // const html = await fetch(base + "formulasymoldes/resina/bridge.html").then(r => r.text());
+  const html = await fetch(url).then(r => r.text());
 
   root.innerHTML = html;
 
-  await LaunchCore.loadCSS(base + "bridge.css");
-  await LaunchCore.loadScript(base + "bridge.js");
+  //await LaunchCore.loadCSS(base + "formulasymoldes/resina/bridge.css");
+  //await LaunchCore.loadScript(base + "formulasymoldes/resina/bridge.js");
+
+  await LaunchCore.loadCSS(url + ".css");
+  await LaunchCore.loadScript(url + ".js");
 
 });
