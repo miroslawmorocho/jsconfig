6d28757d1b3d8afae7dab303534d23a7b6d42a46
Imperio Decorativo <miroslaw_mm@hotmail.com>
2026-03-18 09:43:53 -0500

    cambios

--- core/core-loader.js
+++ core/core-loader.js
@@ -310,7 +310,7 @@ LaunchCore.use = async function(name){
 
 LaunchCore.globals.darkmode = async function(){
 
-  const BASE = "https://miroslawmorocho.github.io/jsconfig/";
+  const BASE = "https://miroslawmorocho.github.io/jsconfig/projects/";
 
   await LaunchCore.loadCSS(BASE+"modules/darkmode/darkmode.css");
 
@@ -323,7 +323,7 @@ LaunchCore.globals.darkmode = async function(){
 
 LaunchCore.globals.carousel = async function(){
 
-  const BASE = "https://miroslawmorocho.github.io/jsconfig/";
+  const BASE = "https://miroslawmorocho.github.io/jsconfig/projects";
 
   const container = document.getElementById("pricing-carousel");
 
@@ -339,7 +339,7 @@ LaunchCore.globals.carousel = async function(){
 
 LaunchCore.globals.scroll = async function(){
 
-  const BASE = "https://miroslawmorocho.github.io/jsconfig/";
+  const BASE = "https://miroslawmorocho.github.io/jsconfig/projects/";
 
   await LaunchCore.loadScript(BASE+"modules/scroll/scroll.js");
 
