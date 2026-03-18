c135cd5961183bbd0dc73cfeb4382364b7c55d29
Imperio Decorativo <miroslaw_mm@hotmail.com>
2026-03-18 02:03:06 -0500

    cambios

--- formulasymoldes/resina/core-loader.js
+++ formulasymoldes/resina/core-loader.js
@@ -289,46 +289,15 @@ LaunchCore.init = async function(){
 /* =====================================================
    MÓDULOS GLOBALES (darkmode, carousel, etc.)
 ===================================================== */
-LaunchCore.globals = {};
+LaunchCore.use = async function(name){
 
-LaunchCore.use = function(name){
   const fn = LaunchCore.globals[name];
-  if(fn) return fn();
-};
 
-LaunchCore.globals.darkmode = async function(){
+  if(!fn){
+    console.warn("Global no encontrado:", name);
+    return;
+  }
 
-  const BASE = "https://miroslawmorocho.github.io/jsconfig/";
-
-  await LaunchCore.loadCSS(BASE+"modules/darkmode/darkmode.css");
-
-  const darkHTML = await fetch(BASE+"modules/darkmode/darkmode.html").then(r=>r.text());
-  document.body.insertAdjacentHTML("beforeend", darkHTML);
-
-  await LaunchCore.loadScript(BASE+"modules/darkmode/darkmode.js");
-
-};
-
-LaunchCore.globals.carousel = async function(){
-
-  const BASE = "https://miroslawmorocho.github.io/jsconfig/";
-
-  const container = document.getElementById("pricing-carousel");
-
-  if(!container) return; // 🔥 importante
-
-  const html = await fetch(BASE+"formulasymoldes/resina/carousel.html").then(r=>r.text());
-  container.innerHTML = html;
-
-  await LaunchCore.loadCSS(BASE+"modules/carousel/carousel.css");
-  await LaunchCore.loadScript(BASE+"modules/carousel/carousel.js");
-
-};
-
-LaunchCore.globals.scroll = async function(){
-
-  const BASE = "https://miroslawmorocho.github.io/jsconfig/";
-
-  await LaunchCore.loadScript(BASE+"modules/scroll/scroll.js");
+  return await fn();
 
 };
