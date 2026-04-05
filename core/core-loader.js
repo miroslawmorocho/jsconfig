afb11b226ade1149b7443c0ba9c8cde70dce0133
Imperio Decorativo <miroslaw_mm@hotmail.com>
2026-04-04 23:07:29 -0500

    updates

--- core/core-loader.js
+++ core/core-loader.js
@@ -1442,8 +1442,8 @@ LaunchCore.globals.versionChecker = async function(){
     versionUrl: dataVersionUrl, // worker data version
     codeVersionUrl, // versión de código estático de Github
     workerUrl: "https://launch-engine.miroslaw-mm.workers.dev",
-    checkInterval: 1*60*1000, // PRODUCCIÓN 15*60*1000 o incluso 60*60*1000,
-    confirmDelay: 1*60*1000, // PRODUCCIÓN 3 * 60 * 1000,
+    checkInterval: 60*60*1000, // PRODUCCIÓN 15*60*1000 o incluso 60*60*1000,
+    confirmDelay: 3*60*1000, // PRODUCCIÓN 3 * 60 * 1000,
     autoReload: true
   });
 
